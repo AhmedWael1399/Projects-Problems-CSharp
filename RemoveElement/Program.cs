8ae7d88758942168bbc280e1b2d92e072f9e2c31
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-15 10:16:02 -0400

    Remove an Element

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution {
+      public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0; 
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = { 3, 2, 2, 3 };
+        int val = 3;
+
+        Solution sol = new Solution();
+        int k = sol.RemoveElement(nums, val);
+
+        Console.WriteLine("New length: " + k);
+        Console.WriteLine("Modified array:");
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+}
