6d5c0c9d969eb3850e821d2c85079de6e540e243
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-16 10:22:20 -0400

    FirstOccurenceString

---
+++ FirstOccurenceString/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (string.IsNullOrEmpty(needle)) return 0;
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            int j = 0;
+            while (j < needle.Length && haystack[i + j] == needle[j])
+            {
+                j++;
+            }
+
+            if (j == needle.Length)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+        string hayStack = "sadbutsad";
+        string needle = "sad";
+        Solution sol = new Solution();
+
+        int index = sol.StrStr(hayStack, needle);
+
+        Console.WriteLine($"The first occurence of the needle is at index {index}");
+    }
+}
