31e26fa2766859b5a998a8615cbd9f03e9302ac9
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-16 11:21:41 -0400

    Search Insert

---
+++ SearchInsert/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return left;
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 2;
+
+        int index = sol.SearchInsert(nums, target);
+        Console.WriteLine($"Number is at index {index}");
+    }
+
+}
