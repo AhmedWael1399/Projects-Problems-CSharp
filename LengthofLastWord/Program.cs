1685cd9a7628d6b8e67e7b04671c19a1f6fdcbd6
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-17 12:30:02 -0400

    Length of Last Word

---
+++ LengthofLastWord/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' ');
+        int i = words.Length - 1;
+        int length = 0;
+        while (i >= 0)
+        {
+            if (words[i] != "") 
+            {
+                length = words[i].Length;
+                return length;
+            }
+            i--;
+        }
+        return 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        string sentence = "   fly me to the moon  ";
+        Solution sol = new Solution();
+/*
+        string[] splits = sentence.Split(' ');
+        foreach (string word in splits)
+        {
+            Console.WriteLine(word);
+        }
+*/
+        int lengthOfLastWord = sol.LengthOfLastWord(sentence);
+
+        Console.WriteLine($"The length of the last word is {lengthOfLastWord}");
+    }
+}
