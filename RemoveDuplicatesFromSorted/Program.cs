650148fd9d3f25be4107c1c6f3809bfdbeae9927
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-21 16:58:58 -0400

    Remove Duplicates from Sorted List

---
+++ RemoveDuplicatesFromSorted/Program.cs
@@ -0,0 +1,55 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        ListNode current = head;
+
+        while (current != null && current.next != null)
+        {
+            if (current.val == current.next.val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+        Console.WriteLine();
+    }
+
+    public static void Main(string[] args)
+    {
+        ListNode list = new ListNode(1,
+                            new ListNode(1,
+                                new ListNode(2)));
+
+        Solution sol = new Solution();
+        ListNode result = sol.DeleteDuplicates(list);
+
+        PrintList(result);
+    }
+}
