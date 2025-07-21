4f14be3ac303bf292231f485c2ff9009fe0cd61f
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-21 16:06:42 -0400

    Climbing Stairs

---
+++ ClimbingStairs/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    public int ClimbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        int n = 5;
+        Console.WriteLine($"The number of ways to climb the steps is {sol.ClimbStairs(n)}");
+    }
+}
