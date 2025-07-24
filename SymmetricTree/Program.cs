e37cd2272f669a5fc4ba36967b7a556a6bcef077
AhmedWael1399 <ahmedeldeebany@gmail.com>
2025-07-24 14:05:17 -0400

    Symmetric Tree

---
+++ SymmetricTree/Program.cs
@@ -0,0 +1,32 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    public bool isMirror(TreeNode leftTree, TreeNode rightTree)
+    {
+        if (leftTree == null && rightTree == null) return true;
+        if (leftTree == null || rightTree == null) return false;
+        if (leftTree.val != rightTree.val) return false;
+
+        return isMirror(leftTree.left, rightTree.right) && isMirror(leftTree.right, rightTree.left);
+
+    }
+
+}
